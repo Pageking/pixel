97bf0f6db58ec246e3897ddfea7af62a6185dcd1
Pageking <info@pageking.nl>
2025-10-03 13:00:50 +0200

    First commit for production flow

---
+++ lib/helpers/get-cw-app-folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+IFS=$'\n'
+set -e
+
+function getAppFolder() {
+	local SERVER_LIST=$(curl -s GET "https://api.cloudways.com/api/v1/server" \
+	-H "Authorization: Bearer $1")
+
+	export APP_FOLDER_NAME=$(echo "$SERVER_LIST" | jq -r --arg SERVER_ID $2 --arg APP_ID $3 '.servers[] | select(.id == ($SERVER_ID)) | .apps[] | select(.id == $APP_ID) | .sys_user')
+}
---
+++ lib/init-prod.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+IFS=$'\n'
+set -e
+
+source "$(dirname "${BASH_SOURCE[0]}")/helpers/check-public-folder.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/helpers/get-project-name.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/helpers/get-cw-bearer.sh"
+check_public_folder
+get_cw_bearer
+
+# SELECT SERVER
+SERVERS=$(curl -s GET "https://api.cloudways.com/api/v1/server" \
+  -H "Authorization: Bearer $ACCESS_TOKEN")
+
+select server in $(echo "$SERVERS" | jq -r '.servers[].label'); 
+do
+	echo "You selected server: $server"
+	# Fetch server details
+	SERVER_DETAILS=$(echo "$SERVERS" | jq -r --arg LABEL "$server" '.servers[] | select(.label == $LABEL)')
+	SERVER_ID=$(echo "$SERVER_DETAILS" | jq -r '.id')
+	break;
+done
+
+NEW_APP=$(curl -s POST "https://api.cloudways.com/api/v1/app" \
+	-H "Authorization: Bearer $ACCESS_TOKEN" \
+	-H "Content-Type: application/json" \
+	-d "{
+		\"server_id\": $SERVER_ID,
+		\"application\": \"wordpress\",
+		\"app_label\": \"Test app (delete)\"
+	}")
+
+echo "$NEW_APP" | jq '.'
+
+if [ "$(echo "$NEW_APP" | jq -r '.status')" != true ]; then
+	echo "Failed to create new app"
+	exit 1
+fi
+
+OP_ID=$(echo "$NEW_APP" | jq -r '.operation_id')
+echo "Operation ID: $OP_ID"
+
+while true; do
+	APP_STATUS=$(curl -s GET "https://api.cloudways.com/api/v1/operation/$OP_ID" \
+		-H "Authorization: Bearer $ACCESS_TOKEN")
+
+	if [ "$(echo "$APP_STATUS" | jq -r '.operation.is_completed')" != "0" ]; then
+		APP_ID=$(echo "$APP_STATUS" | jq -r '.operation.app_id')
+		echo "✅ App created successfully"
+		break
+	fi
+	echo "⏳ App creation in progress..."
+	sleep 5
+done
+
+echo "✅ New app created successfully with ID: $APP_ID"
+
+getAppFolder "$ACCESS_TOKEN" "$SERVER_ID" "$APP_ID"
+echo "App folder name: $APP_FOLDER_NAME"
