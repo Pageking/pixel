f64ef648515321e6de9d87a57369ba0a15c98991
Pageking <info@pageking.nl>
2025-10-06 17:12:41 +0200

    Added missing vars

--- lib/helpers/cw-generate-git-ssh.sh
+++ lib/helpers/cw-generate-git-ssh.sh
@@ -2,7 +2,13 @@
 IFS=$'\n'
 set -e
 
+source "$(dirname "${BASH_SOURCE[0]}")/get-project-name.sh"
+
 cwGenerateGitSSH() {
+	local CONFIG_PATH="$HOME/.config/pixel/config.json"
+	local GITHUB_ORG=$(jq -r '.github.org' "$CONFIG_PATH")
+	local PROJECT_NAME=$(get_project_name)
+
 	local GENERATE_KEY=$(curl -s -X POST "https://api.cloudways.com/api/v1/git/generateKey" \
 		-H "Authorization: Bearer $1" \
 		-H "Content-Type: application/json" \
@@ -10,8 +16,6 @@ cwGenerateGitSSH() {
 			\"server_id\": \"$2\",
 			\"app_id\": \"$3\"
 	}")
-
-	echo $GENERATE_KEY
 
 	local CW_GIT_SSH_KEY=$(curl -s -X GET "https://api.cloudways.com/api/v1/git/key" \
 		-H "Authorization: Bearer $1" \
