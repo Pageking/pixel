e386e6252daed73c72aae719607dc2ecc6c8425f
Pageking <info@pageking.nl>
2025-10-06 11:16:39 +0200

    Added backslashes to parameters

--- lib/helpers/cw-generate-git-ssh.sh
+++ lib/helpers/cw-generate-git-ssh.sh
@@ -3,22 +3,22 @@ IFS=$'\n'
 set -e
 
 cwGenerateGitSSH () {
-	GENERATE_KEY=$(curl -s -X POST "https://api.cloudways.com/api/v1/git/generateKey" \
+	local GENERATE_KEY=$(curl -s -X POST "https://api.cloudways.com/api/v1/git/generateKey" \
 		-H "Authorization: Bearer $1" \
 		-H "Content-Type: application/json" \
 		-d "{
-			\"server_id\": "$2",
-			\"app_id\": "$3"
+			\"server_id\": \"$2\",
+			\"app_id\": \"$3\"
 	}")
 
 	echo $GENERATE_KEY
 
-	CW_GIT_SSH_KEY=$(curl -s -X GET "https://api.cloudways.com/api/v1/git/key" \
+	local CW_GIT_SSH_KEY=$(curl -s -X GET "https://api.cloudways.com/api/v1/git/key" \
 		-H "Authorization: Bearer $1" \
 		-H "Content-Type: application/json" \
 		-d "{
-			\"server_id\": "$2",
-			\"app_id\": "$3"
+			\"server_id\": \"$2\",
+			\"app_id\": \"$3\"
 	}" | jq -r ".key")
 
 	export $CW_GIT_SSH_KEY
