3bf852e49ffc8d5e21b00c19405907bcf639713e
Pageking <info@pageking.nl>
2026-05-06 10:53:03 +0200

    Removed APP_FOLDER_NAME from local declaration because of scoping issues

--- lib/helpers/prod/get-cw-app-folder.sh
+++ lib/helpers/prod/get-cw-app-folder.sh
@@ -3,7 +3,7 @@ IFS=$'\n'
 set -e
 
 getAppFolder() {
-	local SERVER_LIST APP_FOLDER_NAME
+	local SERVER_LIST
 	SERVER_LIST=$(curl -s GET "https://api.cloudways.com/api/v2/server" \
 	-H "Authorization: Bearer $1")
 	APP_FOLDER_NAME=$(echo "$SERVER_LIST" | jq -r --arg SERVER_ID "$2" --arg APP_ID "$3" '.servers[] | select(.id == ($SERVER_ID)) | .apps[] | select(.id == $APP_ID) | .sys_user')
